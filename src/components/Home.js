ab4711b46d106f4b67fbd63172ebb43912979fd3
austinjnr <austinobonyo@gmail.com>
2023-02-06 18:34:55 +0300

    adds clicking event on the home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,17 @@
 const Home = () => {
+
+    let paragraph = 'Working like a sicko this morning I puked on laundry';
+
+
+    const handleClick = () => {
+        paragraph = 'My name is Ian Conner';
+    }
+
     return (  
         <div className="home">
             <h2>Homepage</h2>
+            <p>{ paragraph }</p>
+            <button onClick = {handleClick}>Click Here</button>
         </div>
     );
 }
