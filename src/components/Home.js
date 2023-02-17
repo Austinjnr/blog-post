e92e8e80fefad2dd224793216eed0c9ee0c1e992
austinjnr <austinobonyo@gmail.com>
2023-02-17 13:22:21 +0300

    edited link

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,18 +3,7 @@ import BlogList from "./Bloglist";
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState([
-        {title: 'AIR JORDAN 1 CHICAGO',body:'',author:'Phil Knight',id:1},
-        {title:'NIKE BLAZER MID',body:'',author:'',id:2},
-        {title:'NIKE AIR MAX 95',body:'',author:'',id:3},
-        {title:'NIKE AIR FORCE 1 LOW',body:'',author:'',id:4},
-        {title:'NIKE AIR TRAINER 1',body:'',author:'',id:5},
-        {title:'NIKE CORTEZ',body:'',author:'',id:6},
-        {title:'NIKE AIR ZOOM TEMPO NEXT%',body:'',author:'',id:7},
-        {title:'NIKE DUNK SB LOW',body:'',author:'',id:8},
-        {title:'SACAI X NIKE VAPORWAFFLE',body:'',author:'',id:9},
-        {title:'NIKE AIR MAX PENNY 1',body:'',author:'',id:10}  
-    ]);
+    const [blogs, setBlogs] = useState(null);
 
     const handleDelete = (id) =>{
         const newBlogs = blogs.filter(blog => blog.id !== id);
@@ -22,12 +11,18 @@ const Home = () => {
     }
 
     useEffect(() => {
-        console.log("use effect ran");
+        fetch('https://api.npoint.io/b87269ddd5248acc1302')
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            setBlogs(data);
+        })
     }, [])
 
     return (  
         <div className="home">
-          <BlogList blogs={blogs} title= "Nike Top Sneakers" handleDelete = {handleDelete} />
+           {blogs && <BlogList blogs={blogs} title= "Nike Top Sneakers" handleDelete = {handleDelete} />}
         </div>
     );
 }
